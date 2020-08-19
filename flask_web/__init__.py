09616be5951ef907dc342c50cdfff1b2375eaf59
Kevin Dryfuse <kevin@kevindryfuse.com>
2020-08-19 17:56:53 -0400

    Base bootstrap and theme add

--- flask_web/__init__.py
+++ flask_web/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 from config import Config
+from flask_bootstrap import Bootstrap
+
+bootstrap = Bootstrap()
 
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    bootstrap.init_app(app)
 
     from flask_web.errors import bp as errors_bp
     app.register_blueprint(errors_bp)
